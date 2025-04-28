5f9a81928ed9fcda43d3ab9d3c6d8198665b0873
Alexander Zhura <nice.zhura@list.ru>
2025-04-28 23:46:19 +0300

    Collatz Conjecture & Difference of Squares Done!

--- handmade.el
+++ handmade.el
@@ -41,3 +41,33 @@
 
 (equal (find-binary [1 3 4 6 8 9 11] 6) 3)
 (equal (find-binary [] 6) nil)
+
+
+;; Collatz Conjecture
+(defun steps (number)
+  (let ((result-steps 0))
+    (if (> number 0)
+        (while (/= number 1)
+          (setq number
+                (if (= (mod number 2) 0) (/ number 2)
+                  (1+ (* 3 number))))
+          (setq result-steps (1+ result-steps)))
+      (error "value-error: argument must be positive number"))
+    result-steps))
+
+(equal (steps 1000000) 152)
+(condition-case result-error (steps 0) (error (message "t")))
+
+
+;; Difference of Squares
+(defun sum-of-squares (n)
+  (/ (* n (+ n 1) (+ (* 2 n) 1)) 6))
+
+(defun square-of-sum (n)
+  (expt (/ (* n (1+ n)) 2) 2))
+
+(defun difference (n)
+  (- (square-of-sum n) (sum-of-squares n)))
+
+(equal (difference 10) 2640)
+(equal (difference 100) 25164150)
